7e99d3e28f9f297cb118fdb333d59e2cfeb1a990
jpecoraro342 <jpecoraro342@ufl.edu>
2016-05-19 19:50:00 -0500

    bugfix

--- sqlscripts/queries.sql
+++ sqlscripts/queries.sql
@@ -21,7 +21,7 @@ GROUP BY Posts.pk_postid, Users.pk_userid, Users.username;
 
 -- Get post id
 SELECT Users.pk_userid, Users.username, Posts.pk_postid, Posts.post, Posts.created, Posts.lastmodified,
-json_agg((SELECT r FROM (SELECT Reactions.pk_reactionid, Reactions.reaction) r)) as reactions
+	json_agg((SELECT r FROM (SELECT Reactions.pk_reactionid, Reactions.reaction) r)) as reactions
 FROM Posts
 INNER JOIN Reactions ON Posts.pk_postid = Reactions.fk_postid
 INNER JOIN Users ON Posts.fk_userid = Users.pk_userid
