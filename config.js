b686b81070e29d1256a255660a16d5d19fa335b1
James Risberg <jimmyrisberg56@gmail.com>
2016-11-28 23:37:34 -0800

    Fix postgres url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "port": process.env.PORT || 3000,
     // "postgres_url": "postgres://localhost/emojr",
-    "postgres_url": "ppostgres://tnlvnlppvozawg:29kBksgBhXosbCOdMvFiBP3alD@ec2-54-163-254-231.compute-1.amazonaws.com:5432/df2o769m78f92",
+    "postgres_url": "postgres://tnlvnlppvozawg:29kBksgBhXosbCOdMvFiBP3alD@ec2-54-163-254-231.compute-1.amazonaws.com:5432/df2o769m78f92",
     "sessionCookie": {
         "maxAge": 8640000,
         "httpOnly": true,
